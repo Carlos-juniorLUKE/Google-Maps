9d6ae263850782d412439f28c92229760cafa200
LukeCarlosJr <carlos.ca9974@gmail.com>
2021-09-16 22:22:35 -0300

    feat(search_api, andress_page,place): adicionado paginas ao projeto

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -23,56 +23,53 @@ class _MapPageState extends State<MapPage> {
   void initState() {
     super.initState();
     position = ValueNotifier<LatLng>(LatLng(lat, long));
-    determinePosition().then((value) => position.value = LatLng(value.latitude, value.longitude));
+    determinePosition().then(
+        (value) => position.value = LatLng(value.latitude, value.longitude));
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: TextField(
-            onSubmitted: (val) {
-              lat = -11.396453;
-              long = -37.411528;
-
-              LatLng newPosition = LatLng(lat, long);
-              mapController.moveCamera(CameraUpdate.newLatLng(newPosition));
-
-             
-                position.value = newPosition;
-            },
-          ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          onSubmitted: (val) {
+            lat = -11.396453;
+            long = -37.411528;
+            LatLng newPosition = LatLng(lat, long);
+            mapController.moveCamera(CameraUpdate.newLatLng(newPosition));
+            position.value = newPosition;
+          },
         ),
-        body: Container(child:
-          ValueListenableBuilder<LatLng>(
-            valueListenable:  position,
+      ),
+      body: Container(
+        child: ValueListenableBuilder<LatLng>(
+            valueListenable: position,
             builder: (BuildContext context, LatLng value, Widget child) {
-               final Marker marker = Marker(
+              final Marker marker = Marker(
                   markerId: new MarkerId("123456"),
                   position: value,
                   infoWindow: InfoWindow(
                     title: "Casa De Ruan",
                     snippet: "Santa Luzia/SE",
                   ));
-                markers.add(marker);
+              markers.add(marker);
               return GoogleMap(
-            onMapCreated: _onMapCreated,
-            onCameraMove: (data) {
-              print(data);
-            },
-            onTap: (position) {
-              print(position);
-            },
-            initialCameraPosition: CameraPosition(
-              target: value,
-              zoom: 11.0,
-            ),
-            markers: markers,
-          );}
-        ),
+                onMapCreated: _onMapCreated,
+                onCameraMove: (data) {
+                  print(data);
+                },
+                onTap: (position) {
+                  print(position);
+                },
+                initialCameraPosition: CameraPosition(
+                  target: value,
+                  zoom: 11.0,
+                ),
+                markers: markers,
+              );
+            }),
       ),
-    )
-    );
+    ));
   }
 }
